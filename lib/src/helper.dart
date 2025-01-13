034a4be9ff5395d6bf18510fad0a86d725382eed
fayisdev <fayistkm36@gamil.com>
2025-01-13 18:34:11 +0530

    fix: import fix

--- lib/src/helper.dart
+++ lib/src/helper.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_epub_viewer/src/utils.dart';
 import 'package:http/http.dart' as http;
 
 import 'package:json_annotation/json_annotation.dart';
